6d58116bdb699df933e9b4f45b62275e4eafca86
Nicholas McCarroll <nickmccarroll8@gmail.com>
2024-07-14 22:19:29 -0400

    made stripe account

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express")
 const app = express()
 PORT = 3005
+require('dotenv').config()
+
+//Variables
+const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY
+const stripe = require('stripe')(STRIPE_SECRET_KEY)
+const DOMAIN = "http://localhost:3005"
+
 
 app.use(express.json())
 
@@ -9,6 +16,31 @@ app.use(express.static('public'))
 
 
 //routes
+app.post('/create-checkout-session/:product', (req, res) => {
+  const { product } = req.params
+  let mode, price_ID, line_items
+
+  if (product === 'sub') {
+    price_ID = ''
+    mode = 'subscription'
+    line_items = [
+      {
+        price: price_ID
+      }
+    ]
+  } else if (product === 'pre') {
+    price_ID = ''
+    mode = 'payment'
+    line_items = [
+      {
+        price: price_ID,
+        quantity: 1
+      }
+    ]
+  } else {
+    return res.sendStatus(403)
+  }
+})
 
 
 app.listen(PORT, () => console.log("Listening to port :", PORT))
